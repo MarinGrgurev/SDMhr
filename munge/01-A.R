d5040e981c2b811c1ae9d5faa2479fbb39825ac6
Marin Grgurev <marin.grgurev@gmail.com>
2014-09-12 09:20:58 +0200

    Few changes in reports folder related to creating Rmd document

--- munge/01-A.R
+++ munge/01-A.R
@@ -1,14 +1,17 @@
 # Data preprocessing script.
 ## Adjusting the variable class and names
 ### Changing the name for 3rd, 5th and 6th column
-setnames(data, colnames(data)[c(3,5,6)], c("radiushr", "statistic", "value"))
+setnames(data, colnames(data)[c(2,3,4,5,6)], c("pres.abs", "radius.hr", "ecol.var", "statistic", "value"))
 
 ### Renaming all but lcov values to include word statistic in name
-data[statistic != "percent", ecolvar := paste(data[statistic != "percent", ecolvar], "_", data[statistic != "percent", statistic], sep = "")]
+data[statistic != "percent", ecol.var := paste(data[statistic != "percent", ecol.var], "_", data[statistic != "percent", statistic], sep = "")]
 
 ### Remove "statistic" column as we don't need it any more
 data[,statistic := NULL]
 
 ### Change pres_abs and radiushr columns to factors
-data[,pres_abs := as.factor(pres_abs)]
-data[,radiushr := as.factor(radiushr)]
+data[,pres.abs := as.factor(pres.abs)]
+data[,radius.hr := as.factor(radius.hr)]
+
+### Cache data
+if(!file.exists("cache/data.RData")) {cache("data")}
